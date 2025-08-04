cb65313ebd80bdceec56261288cb4ec236819da7
Saksham Gupta <guptasaksham011@gmail.com>
2025-08-05 00:13:58 +0530

    Updated feature in index.cpp

--- index.cpp
+++ index.cpp
@@ -1,126 +1,145 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-class temp{
-    string userName,Email,Password;
-    string SearchName,SearchPass,SearchEmail;
-
+class Temp {
+    string userName, Email, Password;
+    string SearchName, SearchPass, SearchEmail;
     fstream file;
-    public:
+
+public:
     void login();
     void signUp();
     void forget();
+} obj;
 
-}obj;
+void clearScreen() {
+    cout << "\n-------------------------\n";
+}
 
-int main(){
+int main() {
     char choice;
+    while (true) {
+        clearScreen();
+        cout << "\n1. Login";
+        cout << "\n2. Sign Up";
+        cout << "\n3. Forget Password";
+        cout << "\n4. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-    cout<<"\n-1 Login";
-    cout<<"\n-2 Sign Up";
-    cout<<"\n-3 Forget Password";
-    cout<<"\n-4 Exit";
-    cout<<"\nEnter your choice : ";
-    cin>>choice;
-    cin.ignore();
-
-    switch (choice)
-    {
-    case '1':
-        obj.login();
-        break;
-    
-    case '2':
-        obj.signUp();
-        break;
-
-    case '3':
-        obj.forget();
-        break;
-    
-    case '4':
-        return 0;
-        break;
-    
-    default:
-        cout<<"Invalid expression";
+        switch (choice) {
+            case '1':
+                obj.login();
+                break;
+            case '2':
+                obj.signUp();
+                break;
+            case '3':
+                obj.forget();
+                break;
+            case '4':
+                cout << "Exiting...\n";
+                return 0;
+            default:
+                cout << "Invalid choice.\n";
+        }
     }
-
 
     return 0;
 }
 
-void temp :: signUp(){
-    cout<<"\nEnter your UserName : ";
-    getline(cin,userName);
-    cout<<"Enter your Email : ";
-    getline(cin,Email);
-    cout<<"Enter your Password : ";
-    getline(cin,Password);
+void Temp::signUp() {
+    clearScreen();
+    cout << "=== Sign Up ===\n";
+    cout << "Enter Username: ";
+    getline(cin, userName);
+    cout << "Enter Email: ";
+    getline(cin, Email);
+    cout << "Enter Password: ";
+    getline(cin, Password);
 
-    file.open("loginData.txt",ios :: out | ios :: app);
-    file<<userName<<"*"<<Email<<"*"<<Password<<endl;
+    // Save to file
+    file.open("loginData.txt", ios::app);
+    if (file.is_open()) {
+        file << userName << "*" << Email << "*" << Password << endl;
+        file.close();
+        cout << "\nAccount created successfully.\n";
+    } else {
+        cout << "\nFailed to open file.\n";
+    }
+}
+
+void Temp::login() {
+    clearScreen();
+    cout << "=== Login ===\n";
+    cout << "Enter Username: ";
+    getline(cin, SearchName);
+    cout << "Enter Password: ";
+    getline(cin, SearchPass);
+
+    bool found = false;
+
+    file.open("loginData.txt", ios::in);
+    if (!file) {
+        cout << "No users registered yet.\n";
+        return;
+    }
+
+    while (getline(file, userName, '*') &&
+           getline(file, Email, '*') &&
+           getline(file, Password)) {
+        if (userName == SearchName) {
+            found = true;
+            if (Password == SearchPass) {
+                cout << "\nLogin successful! Welcome, " << userName << ".\n";
+            } else {
+                cout << "Incorrect password.\n";
+            }
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Username not found.\n";
+    }
+
     file.close();
 }
 
-void temp :: login(){
-    
-    cout<<"Enter your UserName : ";
-    getline(cin,SearchName);
-    cout<<"\nEnter your Password : ";
-    getline(cin,SearchPass);
+void Temp::forget() {
+    clearScreen();
+    cout << "=== Forgot Password ===\n";
+    cout << "Enter Username: ";
+    getline(cin, SearchName);
+    cout << "Enter Email: ";
+    getline(cin, SearchEmail);
 
-    file.open("loginData.txt",ios :: in);
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,Password,'\n');
+    bool found = false;
 
-    while(!file.eof()){
-        if(userName == SearchName){
-            if(Password == SearchPass){
-                cout<<"\nAccount Login Successful";
-                cout<<"\nUsername : "<<userName<<endl;
-            }
-            else{
-                cout<<"Password is Incorrect";
-            }
-        }
-        else{
-                cout<<"Incorrect Username";
-            }
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,Password,'\n');
+    file.open("loginData.txt", ios::in);
+    if (!file) {
+        cout << "No users registered yet.\n";
+        return;
     }
-    file.close();
-}
 
-void temp :: forget(){
-
-    cout<<"\nEnter your Username : ";
-    getline(cin,SearchName);
-    cout<<"\nEnter your Email Address : ";
-    getline(cin,SearchEmail);
-
-    file.open("loginData.txt", ios :: in);
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,Password,'\n');
-
-    while(!file.eof()){
-        if(userName == SearchName){
-            if(Email == SearchEmail){
-                cout<<"\nAccount Found...!"<<endl;
-                cout<<"Your Password : "<<Password<<endl;
+    while (getline(file, userName, '*') &&
+           getline(file, Email, '*') &&
+           getline(file, Password)) {
+        if (userName == SearchName) {
+            found = true;
+            if (Email == SearchEmail) {
+                cout << "\nAccount found.\nYour password is: " << Password << endl;
+            } else {
+                cout << "Email does not match our records.\n";
             }
-            else{
-             cout<<"Email Not Found....!\n";   
-            }
-        }
-        else{
-            cout<<"UserName Not Found...!\n";
+            break;
         }
     }
+
+    if (!found) {
+        cout << "Username not found.\n";
+    }
+
     file.close();
 }
